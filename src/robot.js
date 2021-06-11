4495ccc9532642c76a6c899912b8786053e6fc64
Steven <wgyngbh@gmail.com>
2021-06-11 10:24:34 +0800

    update config

--- src/robot.js
+++ src/robot.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 const chalk = require('chalk')
-const request = require('request')
+const rpn = require('request-promise-native') // deprecated!
 const xmlreader = require('xmlreader')
 
 const startURI = 'https://www.sitemaps.org/'
@@ -9,13 +9,55 @@ const siteMap = 'https://www.sitemaps.org/sitemap.xml'
 const dirPrefix = '../temp/'
 const clog = console.log
 
-request(siteMap, function(errors, response, body) {
-  clog(`${siteMap} ${chalk.green('GET')}`)
-  if(errors) throw new Error('请求出错')
-  // clog(response)
-  xmlreader.read(body, function(errors, response) {
-    if(errors) throw new Error('解析出错')
-    // clog(response.urlset)
-    clog(response.urlset.url.array)
-  })
-})
+function getPageName (uri) {
+  if (uri.endsWith('/')) {
+    return `${uri}index.html`.replace(`${startURI}`, '')
+  } else {
+    return uri.replace(`${startURI}`, '')
+  }
+}
+
+function savePageHTML (uri) {
+  const tempPagePath = path.resolve(__dirname, dirPrefix + getPageName(uri))
+  if (!fs.existsSync(tempPagePath)) {
+    return rpn(uri)
+      .then(function (body) {
+        clog(`${uri} ${chalk.green('GET')}`)
+        fs.ensureFileSync(tempPagePath)
+        fs.writeFileSync(tempPagePath, body)
+        clog(`${uri} ${chalk.green('下载成功')}`)
+      })
+      .catch(function (err) {
+        clog(chalk.red(`程序异常:${err}`))
+      })
+  } else {
+    return Promise.resolve(() => {
+      clog(`${uri} ${chalk.yellow('跳过')}`)
+    })
+  }
+}
+
+(() => {
+  rpn(siteMap)
+    .then(function (body) {
+      // clog(body)
+      xmlreader.read(body, function (errors, response) {
+        if (errors) throw new Error('解析出错')
+        // clog(response.urlset)
+        // clog(response.urlset.url.array)
+        const arr = []
+        for (const url of response.urlset.url.array) {
+          // clog(url.loc.text())
+          arr.push(savePageHTML(url.loc.text()))
+        }
+        Promise.all(arr).then(() => {
+          clog(chalk.green('下载完成'))
+        })
+      })
+    })
+    .catch(function (err) {
+      clog(chalk.red(`程序异常: ${err}`))
+    })
+})()
+
+// TODO 串行改并行
